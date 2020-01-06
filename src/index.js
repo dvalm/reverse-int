82e4697de2536b4fa590656bbbbad78d5fac502c
dvalm <45800479+dvalm@users.noreply.github.com>
2020-01-06 22:11:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let str = "" + Math.abs(n);
+  let result = 0;
+  for(let i = 0; i < str.length; i++){
+	result += Math.pow(10,i) * parseInt(str[i]);
+  }
+  return result;
 }
